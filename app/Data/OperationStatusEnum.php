68206e3320f96a07de6c4f4981872a961177a3d8
sw7x <susanthawarnapura@gmail.com>
2023-10-05 18:05:37 +0530

    adding application data types

---
+++ app/Data/OperationStatusEnum.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\Data;
+
+class OperationStatusEnum {
+    const ERROR     = 'error';
+    const SUCCESS   = 'success'; 
+    const WARNING   = 'warning'; 
+    const INFO      = 'info';
+}
