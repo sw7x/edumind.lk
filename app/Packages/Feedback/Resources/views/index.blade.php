fffcec4dc2b9062afd2f8adc4b55f00ef5273ecc
sw7x <susanthawarnapura@gmail.com>
2023-10-23 11:48:01 +0530

    delete app/packages folder

--- app/Packages/Feedback/Resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('feedback::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('feedback.name') !!}
-    </p>
-@endsection
--- app/Packages/Payment/Resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('payment::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('payment.name') !!}
-    </p>
-@endsection
