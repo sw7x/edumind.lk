888137eeadee95edef94e75bb097b236974401aa
sw7x <susanthawarnapura@gmail.com>
2023-10-28 23:03:14 +0530

    scopes move into inside the app/Models folder

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\Scopes\NotCheckoutScope;
+use App\Models\Scopes\NotCheckoutScope;
 
 
 
--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\Scopes\CheckoutScope;
+use App\Models\Scopes\CheckoutScope;
 
 
 class Order extends Model
